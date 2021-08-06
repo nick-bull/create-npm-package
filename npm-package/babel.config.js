5b9379c516176da57af280576cb6416bcb0cd868
Nick Bull <nick@bull.dev>
2021-08-06 10:57:33 +0100

    Added `--experimental-json-modules` flag
    This removes a development dependency in exchange for an experimental feature

--- npm-package/babel.config.js
+++ npm-package/babel.config.js
@@ -1,13 +1,11 @@
-import {sync as loadJson} from 'load-json-file';
+import packageConfig from './package.json';
 
 /** 
  * Converts the "imports" entry of the root-level ./package.json to aliases
  * for use in the `babel-plugin-module-resolver` babel plugin
  */
 const fetchPackageImportsAsAliasMap = () => {
-  const rootPackageInfo = loadJson('./package.json');
-
-  const imports = rootPackageInfo.imports;
+  const imports = packageConfig.imports;
   const importEntries = Object.entries(imports);
 
   const removeTrailingExpansion = (item) => item.replace(/\/\*$/, '');
